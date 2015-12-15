0d75a6afc802d9d793b3cb5d764b87c81e61d29f
anjanjanj <anjanjanj@users.noreply.github.com>
2015-12-15 21:11:41 +0800

    changed range event to include touchstart

--- client/templates/selectRangePopover/selectRangePopover.js
+++ client/templates/selectRangePopover/selectRangePopover.js
@@ -1,5 +1,5 @@
 Template.selectRangePopover.events({
-  "mousedown #rangeSelect": function(event, template) {
+  "touchstart, mousedown #rangeSelect": function(event, template) {
     //console.log($(event.target).attr('data-range'));
     Session.set('threadSearchRange', $(event.target).attr('data-range'));
   }
